29684d0fd6223201c83ef88499b9a0388d58f68f
Samilly <samillynunes19@gmail.com>
2022-01-25 11:40:08 -0300

    App text styles file added

---
+++ lib/presentation/core/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+import 'package:vup/presentation/core/app_colors.dart';
+
+class AppTextStyles {
+  static TextStyle hugeTitleStyle = GoogleFonts.balooBhaijaan(
+    fontSize: 36.sp,
+    color: AppColors.white,
+  );
+
+  static TextStyle largeTitleStyle = GoogleFonts.balooBhaijaan(
+    fontSize: 24.sp,
+    color: AppColors.white,
+  );
+
+  static TextStyle regularLabelStyle = GoogleFonts.balooBhaijaan(
+    fontSize: 14.sp,
+    color: AppColors.white,
+  );
+
+  static TextStyle smallLabelStyle = GoogleFonts.balooBhaijaan(
+    fontSize: 12.sp,
+    color: AppColors.white,
+  );
+
+  static TextStyle smallTitleStyle = GoogleFonts.balooBhaijaan(
+    fontSize: 18.sp,
+    color: AppColors.white,
+  );
+}
