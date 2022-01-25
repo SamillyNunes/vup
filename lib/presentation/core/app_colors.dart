6f3af3d252f27b43668aeefc059c44b6cc6791e9
Samilly <samillynunes19@gmail.com>
2022-01-24 22:57:59 -0300

    Colors modified

--- lib/presentation/core/app_colors.dart
+++ lib/presentation/core/app_colors.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const MaterialColor purple = MaterialColor(
-    0xFF452580,
-    {
-      25: Color(0xFFB289FF),
-      50: Color(0xFF894AFF),
-      100: Color(0xFF6E3BCC),
-      150: Color(0xFF380F85),
-    },
-  );
+  static const Color purpleMain = Color(0xFF452580);
+  static const Color purpleW100 = Color(0xFFB289FF);
+  static const Color purplew200 = Color(0xFF452580);
+  static const Color purplew300 = Color(0xFF6E3BCC);
+  static const Color purplew400 = Color(0xFF380F85);
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
