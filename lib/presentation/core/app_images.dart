26d5937b3a6b710be02546b93787238891dc8254
Samilly Nunes <37520136+SamillyNunes@users.noreply.github.com>
2022-01-25 13:04:10 -0300

    Sn/home view (#1)
    * Starting appbar
    
    * App images file and assets configured in pubspec
    
    * App widget created, sizer lib added
    
    * App text styles file added
    
    * App text styles adjustments
    
    * Home view structure created
    
    * Adjustment on tests
    
    * Bottom menu bar created and adjustments on home view

---
+++ lib/presentation/core/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  // PNG Files
+  static const String logoPng = "assets/images/png/logo.png";
+  static const String girlAvatarPng = "assets/images/png/girl-avatar.png";
+
+  // SVG Files
+  static const String addIcon = "assets/images/svg/add.svg";
+  static const String groupIcon = "assets/images/svg/group.svg";
+  static const String haltersIcon = "assets/images/svg/halters.svg";
+  static const String logoIcon = "assets/images/svg/logo.svg";
+  static const String settingsIcon = "assets/images/svg/settings.svg";
+  static const String walletIcon = "assets/images/svg/wallet.svg";
+}
