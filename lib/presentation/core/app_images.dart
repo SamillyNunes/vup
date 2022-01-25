a522e773bca03a95ede28ac1ee4451f61aedf02d
Samilly <samillynunes19@gmail.com>
2022-01-25 11:18:39 -0300

    App images file and assets configured in pubspec

---
+++ lib/presentation/core/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String logoPng = "assets/images/png/logo.png";
+  static const String addIcon = "assets/images/svg/add.svg";
+  static const String groupIcon = "assets/images/svg/group.svg";
+  static const String haltersIcon = "assets/images/svg/halters.svg";
+  static const String logoIcon = "assets/images/svg/logo.svg";
+  static const String settingsIcon = "assets/images/svg/settings.svg";
+  static const String walletIcon = "assets/images/svg/wallet.svg";
+}
