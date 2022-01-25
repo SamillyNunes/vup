3342130f0ccc0bb6ea37c5705d4705985ef4e9a3
Samilly <samillynunes19@gmail.com>
2022-01-24 22:50:08 -0300

    App colors

---
+++ lib/presentation/core/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const MaterialColor purple = MaterialColor(
+    0xFF452580,
+    {
+      25: Color(0xFFB289FF),
+      50: Color(0xFF894AFF),
+      100: Color(0xFF6E3BCC),
+      150: Color(0xFF380F85),
+    },
+  );
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
